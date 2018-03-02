73668b08b56578ec74884ef3455a27384e8a1c30
Fred Morozov <morozov@fred.md>
2018-03-02 22:52:55 +0300

    .intro--illustration

--- js/scripts.js
+++ js/scripts.js
@@ -13,4 +13,17 @@ $(document).ready(function () {
 		$(this).addClass('is-active');
 	});
 
+	$('body').on('mousemove', function (e) {
+		parallax (e, $('.intro--image.is-buildings')[0], 1);
+		parallax (e, $('.intro--image.is-clouds')[0], 2);
+	});
+
+	function parallax (e, target, layer) {
+		var layer_coeff = 50 / layer;
+		var x = -1 * ((e.pageX - ($(window).width() / 2)) / layer_coeff);
+		var y = -1 * ((e.pageY - ($(window).height() / 2)) / layer_coeff);
+
+		$(target).css('transform', 'translate(' + x + 'px, ' + y + 'px)');
+	};
+
 });
