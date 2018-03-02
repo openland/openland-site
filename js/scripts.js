748e1177eb79c87d5f91d8ccfb22fe697547265a
Fred Morozov <morozov@fred.md>
2018-03-02 16:57:00 +0300

    Icons

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,26 @@
 $(document).ready(function () {
 
-	$('a[href="#"]').on({
-		click: function (e) {
-				e.preventDefault();
+	$('a[href="#"]').on('click', function (e) {
+		e.preventDefault();
 
-				console.log('Click on a[href=#]');
-			}
+		console.log('Click on a[href=#]');
 	});
+
+	$('.map--tab').on('click', function (e) {
+		e.preventDefault();
+
+		$('.map--tab').not(this).removeClass('is-active');
+		$(this).addClass('is-active');
+	});
+
+	//anime({
+	//	targets: '.feature--icon path',
+	//	strokeDashoffset: [anime.setDashoffset, 0],
+	//	easing: 'easeInOutSine',
+	//	duration: 1500,
+	//	delay: function (el, i) { return i * 250 },
+	//	direction: 'alternate',
+	//	loop: true
+	//});
 
 });
